9d3a93d518ee88c55fcf1ee6e36190e5a185be2a
Mikhail Derbichev <arhangel662@gmail.com>
2022-08-25 06:33:21 +0300

    test

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,5 +1,14 @@
 require('dotenv').config();
 
+console.log({
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  })
+
 module.exports = {
   development: {
     dialect: 'sqlite',
